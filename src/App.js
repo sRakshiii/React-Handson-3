15e839d5e465b3519ee5d03b419df531527e2906
sRakshiii <86668172+sRakshiii@users.noreply.github.com>
2023-05-26 12:07:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from "react";
+import App2 from './App2';
+class App extends Component{
+    
+    state={
+            name:"",
+            dpt:"",
+            rating:"",
+            click:true,
+            data:[]
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    handlechange = (event)=>{
+      this.setState({[event.target.name]:event.target.value})
+    }
+
+    togglefunction = () =>{
+      this.setState({click: !this.state.click})
+    }
+
+      
+    handleClick=()=>{
+      const obj={
+            name:this.state.name,
+            dpt:this.state.dpt,
+            rating:this.state.rating
+          }
+      this.state.data.push(obj);
+      this.setState({data:this.state.data,click:false, name: "", dpt: "", rating:""})
+    }
+    render(){
+      return(
+        <>
+        {this.state.click ?
+          <div className="App">
+          <h2>EMPLOYEE FEEDBACK FORM</h2>
+          <br></br>
+          <label>Name :  
+          <input type="text" name="name" onChange={this.handlechange}  value={this.state.name} className="inputBox"/>
+          </label>
+          <br></br>
+          <br></br>
+          <label>Department :  
+          <input type="text" name="dpt" onChange={this.handlechange}  value={this.state.dpt} className="inputBox"/>
+          </label>
+          <br></br>
+          <br></br>
+          <label>Rating :  
+          <input type="text" name="rating" onChange={this.handlechange}  value={this.state.rating} className="inputBox"/>
+          </label>
+          <br></br>
+          <br></br>
+          <button onClick={this.handleClick}>Submit</button>
+          </div>
+          :
+          <App2 value={this.state.data} tf={this.togglefunction}/>
+        }
+        </>
+      )
+    }
 }
-
 export default App;
---
+++ src/App2.js
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+import './App.css';
+
+class App2 extends Component {
+    
+  render() {
+    return (
+        <>
+        <h2 className="headtwo">EMPLOYEE FEEDBACK DATA</h2>
+        <div className="mainBox">
+        {this.props.value.map((item)=>{
+          return(
+            <h4 className="box">Name : {item.name} | Department : {item.dpt} | Rating : {item.rating}</h4>
+          )
+        })}
+        <button onClick={this.props.tf} className="backBtn" >Go Back</button>
+        </div>
+        </>
+    )
+  }
+}
+
+export default App2;
